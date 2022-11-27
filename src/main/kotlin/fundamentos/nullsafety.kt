8c6f4a43ee9aff70a44cedfb0d4ed37759570aa0
DionatanLeao <dionatan.ti@outlook.com>
2022-11-27 14:15:20 -0300

    Null Safety

---
+++ src/main/kotlin/fundamentos/nullsafety.kt
@@ -0,0 +1,20 @@
+package fundamentos
+
+fun main() {
+
+    var lista: List<Int?> = listOf(1, 2, null, 4)
+    var listaNullable: List<Int>? = null
+    
+    var nome: String? = "Dionatan"
+
+    var tamanho: Int = nome?.length ?: 0
+
+    println(tamanho)
+
+    if (nome != null) {
+        println(nome.length)
+    }
+
+    val toShort: Short = nome!!.length.toShort()
+
+}
